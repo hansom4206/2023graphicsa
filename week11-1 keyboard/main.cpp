a4a0db3b6ae41876b6d5896e8cefc5138524c138
eason <95411439+hansom4206@users.noreply.github.com>
2023-04-26 12:08:43 +0800

    Add files via upload

---
+++ week11-1 keyboard/main.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <GL/glut.h>
+void display()
+{
+    glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+void keyboard(unsigned char key,int x,int y)
+{
+    if(key=='1') PlaySound("c:\\Users\\Administrator\\Desktop\\do_re_mi\\do.wav",NULL,SND_ASYNC);
+    if(key=='2') PlaySound("c:\\Users\\Administrator\\Desktop\\do_re_mi\\re.wav",NULL,SND_ASYNC);
+    if(key=='3') PlaySound("c:\\Users\\Administrator\\Desktop\\do_re_mi\\mi.wav",NULL,SND_ASYNC);
+}
+
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week11");
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    glutMainLoop();
+}
